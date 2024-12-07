d24397dcf4cf3e63917523e992f47fc002428e60
KKategor <kamchatkasweethome@gmail.com>
2024-12-07 18:47:33 +0300

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,13 @@
+# Task "Вызов разом"
+
+
+def  apply_all_func(int_list, *functions):
+    dict = {}
+    for func in functions:
+        dict[func.__name__] = func(int_list)
+    return dict
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+
